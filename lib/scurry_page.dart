6aa67fa266ab9addc1139077553bb9624869bf62
Michael Christenson II <m3talsmith@gmail.com>
2023-04-04 15:47:59 -0400

    Round those corners

--- lib/scurry_page.dart
+++ lib/scurry_page.dart
@@ -9,13 +9,23 @@ class ScurryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> children = [
-      Image.file(pic!, fit: BoxFit.fitWidth),
-      Center(child: Text(name, textScaleFactor: 2, softWrap: true,)),
-    ];
-
-    return Stack(
-      children: children,
-    );
+    return Card(
+        elevation: 3.0,
+        child: Stack(children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.file(pic!, fit: BoxFit.fitWidth)),
+          Center(
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(10.0),
+                    color: const Color.fromARGB(95, 255, 255, 255),
+                    child: Text(
+                      name,
+                      textScaleFactor: 2,
+                      softWrap: true,
+                    ))))
+        ]));
   }
 }
