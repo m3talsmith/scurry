974d37e212d903d6208407ef1e08ad251f13b23e
Michael Christenson II <m3talsmith@gmail.com>
2023-04-11 08:27:45 -0400

    Cleaned up Scurry Page

--- lib/scurry_page.dart
+++ lib/scurry_page.dart
@@ -20,25 +20,49 @@ class ScurryPageState extends State<ScurryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Scurry Details'),
-      ),
-      body: FutureBuilder(
-        future: _loadDetails(),
-        builder: (BuildContext context, AsyncSnapshot<Scurry?> snapshot) {
-          if (snapshot.hasData) {
-            scurry = snapshot.data;
-            if (scurry == null) {
-              return const Text('Scurry not found');
-            }
-            // return Text('id: ${widget.id}, name: ${scurry!.name}');
-            return Card(
-            )
-          }
-          return const Center(
-              child: CircularProgressIndicator(
-            value: null,
-          ));
-        }));
+        appBar: AppBar(
+          title: const Text('Scurry Details'),
+        ),
+        body: FutureBuilder(
+            future: _loadDetails(),
+            builder: (BuildContext context, AsyncSnapshot<Scurry?> snapshot) {
+              var width = MediaQuery.of(context).size.width / 3;
+              if (snapshot.hasData) {
+                scurry = snapshot.data;
+                if (scurry == null) {
+                  return const Text('Scurry not found');
+                }
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(padding: EdgeInsets.only(left: 10.0)),
+                    Column(
+                      children: [
+                        ClipOval(
+                          child: Image.file(
+                            scurry!.pic!,
+                            width: width,
+                            height: width,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                        Text(
+                          scurry!.name!,
+                          softWrap: true,
+                          textScaleFactor: 1.5,
+                        ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                    ),
+                  ],
+                );
+              }
+              return const Center(
+                  child: CircularProgressIndicator(
+                value: null,
+              ));
+            }));
   }
 }
